c58ebbc448f5242c8cea9f3963ad2165edb0c478
Simona Badoiu <sbadoiu@cloudflare.com>
2022-03-18 15:24:47 +0000

    ZRZ-205 use client instead of emitter and set/get instead of cookie

--- upward/index.mjs
+++ upward/index.mjs
@@ -1,29 +1,26 @@
 export default function (zaraz) {
   // ====== Subscribe to User-Configured Events ======
   zaraz.addEventListener("event", async (event) => {
-    const { context } = event;
-    const { client } = context;
+    const { client, payload } = event;
 
     // event.data is event.payload.event_type
-    event.payload.tid = client.cookies.upward_tid;
-    const dataToSend = zaraz.utils.filterOutEmptyValues(event.payload); // TODO should we provide some simple `utils`? how do we deal with these?
+    payload.tid = client.get("upward_tid");
+    const dataToSend = zaraz.utils.filterOutEmptyValues(payload); // TODO should we provide some simple `utils`? how do we deal with these?
     if (Object.keys(dataToSend).length) {
-      const params = new URLSearchParams(event.data).toString();
+      const params = new URLSearchParams(payload).toString();
       fetch(`https://www.upward.net/search/u_convert.fsn?${params}`);
     }
   });
 
   // ====== Subscribe to Pageview Events ======
   zaraz.addEventListener("pageview", async (event) => {
-    const { context, emitter } = event;
-    const { client } = context; // TODO client is replacing the current system object? right?
+    const { client } = event;
 
     const tid = client.page.query.tid;
-    if (emitter.type === "browser") {
-      emitter.setCookie("upward_tid", tid, {
-        Path: "/",
-        Expires: "Fri, 31 Dec 2028 23:59:59 GMT",
-      }); // TODO: what are the options, does this look ok?
+    if (client.type === "browser") {
+      client.set("upward_tid", tid, {
+        scope: "infinite",
+      });
     }
   });
 }
