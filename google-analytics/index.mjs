e2a68a9558503926416949469f11b48f975f866f
Simona Badoiu <sbadoiu@cloudflare.com>
2022-03-21 15:51:08 +0000

    ZRZ-207 add google analytics

---
+++ google-analytics/index.mjs
@@ -0,0 +1,35 @@
+export default function (zaraz) {
+  // ====== Subscribe to User-Configured Events ======
+  zaraz.addEventListener("event", async (event) => {
+    const { client, payload } = event;
+
+    payload.tid = client.get("upward_tid");
+
+    for (let param in payload) {
+      if (
+        payload[param] === undefined ||
+        payload[param] === null ||
+        payload[param] === ""
+      ) {
+        delete payload[param];
+      }
+    }
+
+    if (Object.keys(payload).length) {
+      const params = new URLSearchParams(payload).toString();
+      fetch(`https://www.upward.net/search/u_convert.fsn?${params}`);
+    }
+  });
+
+  // ====== Subscribe to Pageview Events ======
+  zaraz.addEventListener("pageview", async (event) => {
+    const { client } = event;
+
+    const tid = client.page.query.tid;
+    if (client.type === "browser" && tid) {
+      client.set("upward_tid", tid, {
+        scope: "infinite",
+      });
+    }
+  });
+}
