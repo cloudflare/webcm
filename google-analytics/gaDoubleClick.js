dde51e3138eb9ef08c118d9b30076e7f487ce7f1
Simona Badoiu <sbadoiu@cloudflare.com>
2022-03-23 12:16:01 +0000

    ZRZ-207 add ga doubleclick and ga audiences support

--- google-analytics/gaDoubleClick.js
+++ google-analytics/gaDoubleClick.js
@@ -1,16 +1,38 @@
 export default gaDoubleClick = ({ payload, client }, finalUrl, settings) => {
-  if (settings["ga-audiences"] || settings["ga-doubleclick"]) {
-    const analyticsURL = new URL(finalUrl);
-    const jid = analyticsURL.searchParams.get("jid");
-    const gjid = analyticsURL.searchParams.get("gjid");
-    const cid = analyticsURL.searchParams.get("cid");
+  const analyticsURL = new URL(finalUrl);
+  const jid = analyticsURL.searchParams.get("jid");
+  const gjid = analyticsURL.searchParams.get("gjid");
+  const cid = analyticsURL.searchParams.get("cid");
 
-    // Build the DoubleClick request first, because it's also needed for GA-Audiences
-    const baseDoubleClick = "https://stats.g.doubleclick.net/j/collect?";
-    const doubleClick = {
-      t: "dc",
+  // Build the DoubleClick request first, because it's also needed for GA-Audiences
+  const baseDoubleClick = "https://stats.g.doubleclick.net/j/collect?";
+  const doubleClick = {
+    t: "dc",
+    aip: 1,
+    _r: 3,
+    v: 1,
+    _v: "j86",
+    tid: payload.tid,
+    cid: cid,
+    jid: jid,
+    gjid: gjid,
+    _u: "KGDAAEADQAAAAC~",
+    z: Math.floor(2147483647 * Math.random()),
+  };
+
+  const doubleClickParams = new URLSearchParams(doubleClick).toString();
+  const finalDoubleClickURL = baseDoubleClick + doubleClickParams;
+
+  if (
+    settings["ga-audiences"] &&
+    (!client.get["_z_ga_audiences"] || client.get["_z_ga_audiences"] !== cid)
+  ) {
+    // Build the GA Audiences request
+    const audiences = {
+      t: "sr",
       aip: 1,
-      _r: 3,
+      _r: 4,
+      slf_rd: 1,
       v: 1,
       _v: "j86",
       tid: payload.tid,
@@ -20,51 +42,27 @@ export default gaDoubleClick = ({ payload, client }, finalUrl, settings) => {
       _u: "KGDAAEADQAAAAC~",
       z: Math.floor(2147483647 * Math.random()),
     };
+    const audienceParams = new URLSearchParams(audiences).toString();
+    const baseAudienceURL = "https://www.google.com/ads/ga-audiences?";
+    const finalAudienceURL = baseAudienceURL + audienceParams;
+    let clientJSAudience = "";
+    // Call GA-Audiences on Google.com
+    client.fetch(finalAudienceURL);
+    client.set("_z_ga_audiences", cid, { scope: "infinite" });
 
-    const doubleClickParams = new URLSearchParams(doubleClick).toString();
-    const finalDoubleClickURL = baseDoubleClick + doubleClickParams;
+    // Trigger the DoubleClick with XHR because we need the response text - it holds the local Google domain
+    clientJSAudience += `x=new XMLHttpRequest,x.withCredentials=!0,x.open("POST","${finalDoubleClickURL}",!0),x.onreadystatechange=function(){`;
+    clientJSAudience += `if (4 == x.readyState) {`;
+    clientJSAudience += `const domain = x.responseText.trim();`;
+    clientJSAudience += `if (domain.startsWith("1g") && domain.length > 2) {`;
 
-    if (
-      settings["ga-audiences"] &&
-      (!client.get["_z_ga_audiences"] || client.get["_z_ga_audiences"] !== cid)
-    ) {
-      // Build the GA Audiences request
-      const audiences = {
-        t: "sr",
-        aip: 1,
-        _r: 4,
-        slf_rd: 1,
-        v: 1,
-        _v: "j86",
-        tid: payload.tid,
-        cid: cid,
-        jid: jid,
-        gjid: gjid,
-        _u: "KGDAAEADQAAAAC~",
-        z: Math.floor(2147483647 * Math.random()),
-      };
-      const audienceParams = new URLSearchParams(audiences).toString();
-      const baseAudienceURL = "https://www.google.com/ads/ga-audiences?";
-      const finalAudienceURL = baseAudienceURL + audienceParams;
-      let clientJSAudience = "";
-      // Call GA-Audiences on Google.com
-      client.fetch(finalAudienceURL);
-      client.set("_z_ga_audiences", cid, { scope: "infinite" });
-
-      // Trigger the DoubleClick with XHR because we need the response text - it holds the local Google domain
-      clientJSAudience += `x=new XMLHttpRequest,x.withCredentials=!0,x.open("POST","${finalDoubleClickURL}",!0),x.onreadystatechange=function(){`;
-      clientJSAudience += `if (4 == x.readyState) {`;
-      clientJSAudience += `const domain = x.responseText.trim();`;
-      clientJSAudience += `if (domain.startsWith("1g") && domain.length > 2) {`;
-
-      // Trigger the request to the local Google domain too
-      clientJSAudience += `zaraz.f("${finalAudienceURL}".replace("www.google.com", "www.google."+domain.slice(2)));`;
-      clientJSAudience += `}}`;
-      clientJSAudience += `},x.send();`;
-      client.eval(clientJSAudience);
-    } else {
-      // If no GA-Audiences, just trigger DoubleClick normally
-      client.fetch(finalDoubleClickURL);
-    }
+    // Trigger the request to the local Google domain too
+    clientJSAudience += `zaraz.f("${finalAudienceURL}".replace("www.google.com", "www.google."+domain.slice(2)));`;
+    clientJSAudience += `}}`;
+    clientJSAudience += `},x.send();`;
+    client.eval(clientJSAudience);
+  } else {
+    // If no GA-Audiences, just trigger DoubleClick normally
+    client.fetch(finalDoubleClickURL);
   }
 };
