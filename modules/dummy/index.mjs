8386484f3ed3ec0f3c7f49e1df41d74ca07fc866
Yo'av Moshe <yoav@cloudflare.com>
2022-04-11 17:02:10 +0200

    add dummy module for testing

---
+++ modules/dummy/index.mjs
@@ -0,0 +1,29 @@
+export default async function (manager) {
+  manager.addEventListener('mousedown', async event => {
+    // Save mouse coordinates as a cookie
+    const { client, payload } = event
+    client.set('lastClickX', payload.clientX)
+    client.set('lastClickY', payload.clientY)
+  })
+
+  manager.addEventListener('event', async event => {
+    // Forward events to vendor
+    const { client, payload } = event
+    payload.user_id = client.get('user_id')
+
+    if (Object.keys(payload).length) {
+      const params = new URLSearchParams(payload).toString()
+      fetch(`https://www.example.com/?${params}`)
+    }
+  })
+
+  manager.addEventListener('pageview', async event => {
+    // Set a user_id based on a query param
+    const { client } = event
+
+    const user_id = client.page.query.user_id
+    client.set('user_id', user_id, {
+      scope: 'infinite',
+    })
+  })
+}
