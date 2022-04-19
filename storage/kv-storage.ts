afb5a6dcfaf6104729d0ab8c60c1819066072572
Yo'av Moshe <yoav@cloudflare.com>
2022-04-19 15:31:17 +0200

    preserve the value type when using the KV storage

--- storage/kv-storage.ts
+++ storage/kv-storage.ts
@@ -3,10 +3,10 @@ import { writeFileSync, readFileSync } from 'fs'
 const BASE_DIR = 'kv'
 
 export const set = (key: string, value: any) => {
-  writeFileSync(BASE_DIR + '/' + key, value)
+  writeFileSync(BASE_DIR + '/' + key, JSON.stringify(value))
   return true
 }
 
 export const get = (key: string) => {
-  return readFileSync(BASE_DIR + '/' + key)
+  return JSON.parse(readFileSync(BASE_DIR + '/' + key).toString())
 }
