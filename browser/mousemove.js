a08f0e5eca50ccc1485915d637d7e1fe7e618217
Ruskin Constant <ruskin@cloudflare.com>
2022-04-21 12:23:02 +0100

    send mousemove payload as array

--- browser/mousemove.js
+++ browser/mousemove.js
@@ -8,18 +8,20 @@ const round = (n, decPlaces = 4) => {
 
 const getMousePosition = async event => {
   const { clientX, clientY, pageX, pageY, target } = event
-
-  const rect = target.getBoundingClientRect()
-  const { left, top, width, height } = rect
+  const { left, top, width, height } = target.getBoundingClientRect()
 
   const payload = {
-    clientX,
-    clientY,
-    pageX,
-    pageY,
-    target,
-    relativeX: round(((clientX - left) / width) * 100),
-    relativeY: round(((clientY - top) / height) * 100),
+    mousemoves: [
+      {
+        clientX,
+        clientY,
+        pageX,
+        pageY,
+        target,
+        relativeX: round(((clientX - left) / width) * 100),
+        relativeY: round(((clientY - top) / height) * 100),
+      },
+    ],
   }
   const res = await fetch(ec._systemEventsPath, {
     method: 'POST',
