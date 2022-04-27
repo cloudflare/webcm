63aeed2359589cd978a2acfa0ed632093d7a170c
Yo'av Moshe <yoav@cloudflare.com>
2022-04-27 14:36:50 +0200

    have two layered client and manager classes, generic + exposed

--- lib/client.ts
+++ lib/client.ts
@@ -1,51 +1,103 @@
 import { Request, Response } from 'express'
+// import parseCookies from '../utils/parseCookies'
+import Cookies from 'cookies'
+import config from '../config.json'
+import { ManagerGeneric } from './manager'
 
-const parseCookies = (cookieString?: string) => {
-  if (!cookieString) return {}
-  return Object.fromEntries(
-    cookieString
-      .replaceAll('; ', ';')
-      .split(';')
-      .map(cookie => cookie.split('='))
-  )
-}
+export class ClientGeneric {
+  type: string
+  page: any
+  request: Request
+  response: Response
+  manager: ManagerGeneric
+  url: any
+  cookies: any
+  webcmPrefs: any
 
-export interface MCClient {
-  page: {
-    query: {
-      [k: string]: string
+  constructor(request: Request, response: Response, manager: ManagerGeneric) {
+    this.type = 'browser'
+    this.manager = manager
+    this.request = request
+    this.response = response
+    this.url = new URL(config.target + request.url)
+    this.cookies = new Cookies(request, response, { keys: [config.cookiesKey] })
+    if (this.cookies.get('webcm_prefs', { signed: !!config.cookiesKey })) {
+      this.webcmPrefs = JSON.parse(
+        this.cookies.get('webcm_prefs', { signed: !!config.cookiesKey })
+      )
+    } else {
+      this.webcmPrefs = { listeners: {} }
     }
   }
-  type: string
-  eval: (code: string) => void
-  return: (value: unknown) => void
-  fetch: (resource: string, settings: any) => void
-  set: (key: string, value: any) => void
-  get: (key: string) => any
+
+  eval(code: string) {
+    this.response.payload.eval.push(code)
+  }
+  return(value: unknown) {
+    this.response.payload.return = value
+  }
+  fetch(resource: string, settings: any) {
+    this.response.payload.fetch.push([resource, settings])
+  }
+  set(key: string, value: any) {
+    this.cookies.set(key, value, { signed: !!config.cookiesKey })
+  }
+  get(key: string) {
+    return this.cookies.get(key, { signed: !!config.cookiesKey })
+  }
+  addEventListener(component: string, event: string, handler: Function) {
+    console.log('called for ', event)
+    // console.log(this.webcmPrefs)
+    if (!this.webcmPrefs.listeners[component]) {
+      this.webcmPrefs.listeners[component] = [event]
+    } else {
+      this.webcmPrefs.listeners[component].push(event)
+    }
+    this.manager.clientListeners[`${event}__${component}`] = handler
+
+    this.cookies.set('webcm_prefs', JSON.stringify(this.webcmPrefs), {
+      signed: true,
+    })
+  }
 }
 
-export const buildClient = (req: Request, res: Response): MCClient => {
-  const url = new URL(req.fullUrl)
-  const cookies = parseCookies(req.headers['cookie'])
-  return {
-    page: {
-      query: Object.fromEntries(url.searchParams),
-    },
-    type: 'browser',
-    eval: (code: string) => {
-      res.payload.eval.push(code)
-    },
-    return: (value: unknown) => {
-      res.payload.return = value
-    },
-    fetch: (resource: string, settings: any) => {
-      res.payload.fetch.push([resource, settings])
-    },
-    set: (key: string, value: any) => {
-      res.append('set-cookie', `${key}=${value}`)
-    },
-    get: (key: string) => {
-      return cookies[key]
-    },
+export class Client {
+  #generic: ClientGeneric
+  #component: string
+  url: URL
+  emitter: string
+
+  constructor(component: string, generic: ClientGeneric) {
+    this.#generic = generic
+    this.#component = component
+    this.url = this.#generic.url
+    this.emitter = 'browser'
+  }
+
+  eval(...args: any) {
+    //@ts-ignore
+    this.#generic.eval(this.#component, ...args)
+  }
+
+  return(...args: any) {
+    //@ts-ignore
+    this.#generic.return(this.#component, ...args)
+  }
+  fetch(...args: any) {
+    //@ts-ignore
+    this.#generic.fetch(this.#component, ...args)
+  }
+  get(key: string) {
+    //@ts-ignore
+    return this.#generic.get(this.#component + '__' + key)
+  }
+  set(key: string, value: any) {
+    //@ts-ignore
+    this.#generic.set(this.#component + '__' + key, value)
+  }
+
+  addEventListener(...args: any) {
+    //@ts-ignore
+    this.#generic.addEventListener(this.#component, ...args)
   }
 }
--- lib/manager.ts
+++ lib/manager.ts
@@ -3,7 +3,9 @@ import path from 'path'
 import { get, set } from '../storage/kv-storage'
 import { useCache } from '../cache/index'
 import { JSDOM } from 'jsdom'
-import { MCClient } from './client'
+import { ClientGeneric } from './client'
+
+console.info('\nWebCM, version', process.env.npm_package_version)
 
 declare global {
   interface Event {
@@ -16,28 +18,27 @@ export interface ComponentSettings {
   [key: string]: any
 }
 
-type EmbedCallback = (contex: { parameters: { [k: string]: unknown } }) => any
+type EmbedCallback = (contex: {
+  parameters: { [k: string]: unknown }
+  client: ClientGeneric
+}) => any
 
 type ComponentConfig = string | ComponentSettings
 
 const EXTS = ['.ts', '.mts', '.mjs', '.js']
-export class Manager extends EventTarget {
+
+export class ManagerGeneric extends EventTarget {
   components: ComponentConfig[]
   trackPath: string
+  name: string
   systemEventsPath: string
   sourcedScript: string
   requiredSnippets: string[]
+  clientListeners: any
   registeredEmbeds: {
     [k: string]: EmbedCallback
   }
-  set: (key: string, value: any) => boolean
-  get: (key: string) => any
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  useCache: (key: string, callback: Function, expiry?: number) => any
-
   constructor(Context: {
-    set?: (key: string, value: any) => boolean
-    get?: (key: string) => any
     components: ComponentConfig[]
     trackPath: string
     systemEventsPath: string
@@ -45,19 +46,20 @@ export class Manager extends EventTarget {
     useCache?: (key: string, callback: Function, expiry?: number) => any
   }) {
     super()
-    this.sourcedScript = "console.log('ecweb script is sourced again')"
+    this.sourcedScript = "console.log('WebCM script is sourced again')"
     this.requiredSnippets = ['track']
     this.registeredEmbeds = {}
+    this.clientListeners = {}
+    this.name = 'WebCM'
     this.trackPath = Context.trackPath
     this.systemEventsPath = Context.systemEventsPath
-    this.set = Context.set || set
-    this.get = Context.get || get
     this.components = Context.components
-    this.useCache = Context.useCache || useCache
     this.initScript()
   }
 
+  // @ts-ignore
   addEventListener(
+    component: string,
     type: string,
     callback: EventListenerOrEventListenerObject | null,
     options?: boolean | AddEventListenerOptions
@@ -65,7 +67,7 @@ export class Manager extends EventTarget {
     if (!this.requiredSnippets.includes(type)) {
       this.requiredSnippets.push(type)
     }
-    super.addEventListener(type, callback, options)
+    super.addEventListener(component + '__' + type, callback, options)
   }
 
   async initScript() {
@@ -96,11 +98,14 @@ export class Manager extends EventTarget {
 
       if (component) {
         try {
-          console.info('loading component', componentName)
-          await component.default(this, componentSettings)
+          console.info(':: Loading component', componentName)
+          await component.default(
+            new Manager(componentName, this),
+            componentSettings
+          )
         } catch (error) {
           console.error(
-            'Error loading component',
+            ':: Error loading component',
             componentPath,
             component,
             error
@@ -110,9 +115,15 @@ export class Manager extends EventTarget {
     }
   }
 
-  getInjectedScript() {
+  getInjectedScript(clientGeneric: ClientGeneric) {
     let injectedScript = ''
-    for (const snippet of this.requiredSnippets) {
+
+    const clientListeners: Set<any> = new Set(
+      Object.entries(clientGeneric.webcmPrefs.listeners)
+        .map(x => x[1])
+        .flat()
+    )
+    for (const snippet of [...this.requiredSnippets, ...clientListeners]) {
       const snippetPath = `browser/${snippet}.js`
       if (existsSync(snippetPath)) {
         injectedScript += readFileSync(snippetPath)
@@ -124,7 +135,7 @@ export class Manager extends EventTarget {
     return injectedScript
   }
 
-  async processEmbeds(response: string, client: MCClient) {
+  async processEmbeds(response: string, client: ClientGeneric) {
     const dom = new JSDOM(response)
     for (const div of dom.window.document.querySelectorAll(
       'div[data-component-embed]'
@@ -143,8 +154,37 @@ export class Manager extends EventTarget {
 
     return dom.serialize()
   }
+}
+
+export class Manager {
+  #generic: ManagerGeneric
+  #component: string
+  name: string
+
+  constructor(component: string, generic: ManagerGeneric) {
+    this.#generic = generic
+    this.#component = component
+    this.name = this.#generic.name
+  }
+
+  addEventListener(...args: any) {
+    // @ts-ignore
+    this.#generic.addEventListener(this.#component, ...args)
+  }
+
+  get(key: string) {
+    get(this.#component + '__' + key)
+  }
+
+  set(key: string, value: any) {
+    set(this.#component + '__' + key, value)
+  }
+
+  async useCache(key: string, callback: Function, expiry?: number) {
+    await useCache(this.#component + '__' + key, callback, expiry)
+  }
 
   registerEmbed(name: string, callback: EmbedCallback) {
-    this.registeredEmbeds[name] = callback
+    this.#generic.registeredEmbeds[this.#component + '__' + name] = callback
   }
 }
