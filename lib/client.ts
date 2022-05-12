98a11966085208b48cc230fee50ee8faf2d193d6
Ruskin Constant <6400000+jonnyparris@users.noreply.github.com>
2022-05-12 11:07:54 +0100

    only expose headers from client, not the full request

--- lib/client.ts
+++ lib/client.ts
@@ -83,13 +83,13 @@ export class Client {
     }
   }
   url: URL
-  request: Request
+  headers: Request['headers']
   emitter: string
 
   constructor(component: string, generic: ClientGeneric) {
     this.#generic = generic
     this.#component = component
-    this.request = this.#generic.request
+    this.headers = this.#generic.request.headers
     this.url = this.#generic.url
     this.page = this.#generic.page
     this.emitter = 'browser'
