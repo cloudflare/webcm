1f96e549e363e0993a45d16a9b7d6acf61a3b00c
Ruskin Constant <6400000+jonnyparris@users.noreply.github.com>
2022-05-13 18:10:51 +0100

    add pageview listener too

--- components/linkedin/index.ts
+++ components/linkedin/index.ts
@@ -2,7 +2,23 @@ import { ComponentSettings, Manager } from '../../lib/manager'
 
 const TRACK_URL = 'https://px.ads.linkedin.com/collect/'
 
-export default async function (manager: Manager, _settings: ComponentSettings) {
+export default async function (manager: Manager, { pid }: ComponentSettings) {
+  manager.addEventListener('pageview', async event => {
+    const payload = {
+      fmt: 'js',
+      v: 2,
+      url: event.client.url.href,
+      time: new Date().valueOf(),
+      pid,
+      ...event.payload,
+    }
+
+    if (Object.keys(payload).length) {
+      const params = new URLSearchParams(payload).toString()
+      event.client.fetch(`${TRACK_URL}?${params}`)
+    }
+  })
+
   manager.addEventListener('event', async event => {
     const payload = {
       fmt: 'js',
