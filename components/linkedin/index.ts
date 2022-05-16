759719c5c0f456e69fd20c526a3b8964266fd0ab
Ruskin Constant <6400000+jonnyparris@users.noreply.github.com>
2022-05-16 14:05:20 +0100

    ensure pid is sent with all events, not just pageviews

--- components/linkedin/index.ts
+++ components/linkedin/index.ts
@@ -1,36 +1,27 @@
-import { ComponentSettings, Manager } from '../../lib/manager'
+import { ComponentSettings, Manager, MCEvent } from '../../lib/manager'
 
 const TRACK_URL = 'https://px.ads.linkedin.com/collect/'
 
+// The pid query parameter, when used alone, is a pageview.
+// Together with a conversionId it becomes a conversion event.
+// conversionIds are typically supplied per 'event'
+const handler = (pid: string) => async (event: MCEvent) => {
+  const payload = {
+    fmt: 'js',
+    v: 2,
+    url: event.client.url.href,
+    time: new Date().valueOf(),
+    pid,
+    ...event.payload,
+  }
+
+  if (Object.keys(payload).length) {
+    const params = new URLSearchParams(payload).toString()
+    event.client.fetch(`${TRACK_URL}?${params}`)
+  }
+}
+
 export default async function (manager: Manager, { pid }: ComponentSettings) {
-  manager.addEventListener('pageview', async event => {
-    const payload = {
-      fmt: 'js',
-      v: 2,
-      url: event.client.url.href,
-      time: new Date().valueOf(),
-      pid,
-      ...event.payload,
-    }
-
-    if (Object.keys(payload).length) {
-      const params = new URLSearchParams(payload).toString()
-      event.client.fetch(`${TRACK_URL}?${params}`)
-    }
-  })
-
-  manager.addEventListener('event', async event => {
-    const payload = {
-      fmt: 'js',
-      v: 2,
-      url: event.client.url.href,
-      time: new Date().valueOf(),
-      ...event.payload,
-    }
-
-    if (Object.keys(payload).length) {
-      const params = new URLSearchParams(payload).toString()
-      event.client.fetch(`${TRACK_URL}?${params}`)
-    }
-  })
+  manager.addEventListener('pageview', handler(pid))
+  manager.addEventListener('event', handler(pid))
 }
