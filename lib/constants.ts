d5061ce677bd3c250bb24368d9ea1f5f6ec32a4d
Simona Badoiu <sbadoiu@cloudflare.com>
2022-06-23 15:21:53 +0100

    set same permission for set and get

--- lib/constants.ts
+++ lib/constants.ts
@@ -6,7 +6,7 @@ export const PERMISSIONS: { [k: string]: string } = {
   // client permissions
   clientGet: 'access_client_kv',
   clientExtGet: 'access_extended_client_kv',
-  clientSet: 'client_kv',
+  clientSet: 'access_client_kv',
   clientExecute: 'execute_unsafe_scripts',
   clientFetch: 'client_network_requests',
 }
